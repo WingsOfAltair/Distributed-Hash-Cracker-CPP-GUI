e891f2cb1bd261d0bdfe0c337f8ed8b9ba02a4cd
Bashar M. Bataineh <basharbataineh@hotmail.com>
2025-07-22 21:09:54 +0300

    Added app icon for server GUI.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icons/appicon.ico"));
     QApplication::setStyle(QStyleFactory::create("Fusion"));
 
     MainWindow window;
